589b83763b672e012c7ea89eac79a9cb6d824a85
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-11 13:26:33 -0500

    :construction: work

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({ required: true })
   name: string;
+
+  @Prop()
   description: string;
+
+  @Prop({ type: Number })
   price: number;
+
+  @Prop({ type: Number })
   stock: number;
+
+  @Prop()
   image: string;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ProductsController } from './controllers/products.controller';
 import { BrandsController } from './controllers/brands.controller';
@@ -6,8 +7,17 @@ import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
+import { Product, ProductSchema } from './entities/product.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
