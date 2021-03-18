79b52f9ee269f4d7c0e9f646556c7f78834bb428
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-18 11:43:12 -0500

    :construction: worl

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,8 +11,6 @@ import { enviroments } from './enviroments';
 import { AuthModule } from './auth/auth.module';
 import config from './config';
 
-
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -21,7 +18,7 @@ import config from './config';
       load: [config],
       isGlobal: true,
       validationSchema: Joi.object({
-        API_KEY: Joi.number().required(),
+        API_KEY: Joi.string().required(),
         DATABASE_NAME: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
       }),
