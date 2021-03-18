2c12780f4ab7b8191bc90d6bfa5e991d8260bf54
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-18 11:25:24 -0500

    :construction: work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,29 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+  SetMetadata,
+} from '@nestjs/common';
+import { Public } from './auth/decorators/public.decorator';
 
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { AppService } from './app.service';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @UseGuards(ApiKeyGuard)
   @Get('nuevo')
+  @Public()
   newEndpoint() {
     return 'yo soy nuevo';
   }
