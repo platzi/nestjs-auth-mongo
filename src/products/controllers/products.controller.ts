9d3dc7502a667264e2b52257084da874125a1d07
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-19 09:16:05 -0500

    :construction: work

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -10,10 +10,12 @@ import {
   HttpStatus,
   HttpCode,
   Res,
+  UseGuards
   // ParseIntPipe,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import { MongoIdPipe } from './../../common/mongo-id.pipe';
@@ -24,6 +26,7 @@ import {
 } from '../dtos/products.dtos';
 import { ProductsService } from './../services/products.service';
 
+@UseGuards(AuthGuard('jwt'))
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
